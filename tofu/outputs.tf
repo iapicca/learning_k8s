55150cb1743450b3fa7f21d92cf01d7812ffb8c9
iapicca <francesco@yakforward.com>
2024-08-22 17:55:58 +0300

    map nodes ips names to hosts' /etc/hosts

--- tofu/outputs.tf
+++ tofu/outputs.tf
@@ -2,10 +2,7 @@ output "master_node_ip" {
   value = digitalocean_droplet.master_node.ipv4_address
 }
 
-output "worker_node_1_ip" {
-  value = digitalocean_droplet.worker_node_1.ipv4_address
-}
-
-output "worker_node_2_ip" {
-  value = digitalocean_droplet.worker_node_2.ipv4_address
+output "worker_node_ips" {
+  description = "The IPv4 addresses of the worker nodes"
+  value       = [for worker in digitalocean_droplet.worker_node : worker.ipv4_address]
 }
---
+++ tofu/provisioners.tf
@@ -0,0 +1,31 @@
+locals {  
+  all_nodes = flatten([
+    digitalocean_droplet.master_node,
+    digitalocean_droplet.worker_node,
+  ]) 
+}
+
+resource "null_resource" "all_nodes_provisioner" {
+  depends_on = [local.all_nodes]
+  count = length(local.all_nodes)
+
+  connection {
+    type        = "ssh"
+    user        = "root"
+    private_key = file("/Users/francesco/.ssh/do_ssh_key")
+    host        = local.all_nodes[count.index].ipv4_address
+  }
+
+  provisioner "file" {
+    source      = "taskfiles/taskfile.yml"
+    destination = "/root/taskfile.yml"
+  }
+
+  provisioner "remote-exec" {
+    inline = flatten([
+      "sudo snap install task --classic",
+      "task setup-kubernetes",
+      [for node in  local.all_nodes : "sudo sh -c 'echo \"${node.ipv4_address} ${node.name}\" >> /etc/hosts'"]
+    ])
+  }
+}
