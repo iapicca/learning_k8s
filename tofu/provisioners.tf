d3cc442a25caf4edb6b08dad6454c06a2af864d7
iapicca <francesco@yakforward.com>
2024-08-23 15:10:57 +0300

    fix taskfile and provisioner script

--- tofu/provisioners.tf
+++ tofu/provisioners.tf
@@ -23,10 +23,12 @@ resource "null_resource" "all_nodes_provisioner" {
 
   provisioner "remote-exec" {
     inline = flatten([
-      "curl -sL https://taskfile.dev/install.sh | sh",
-      "task setup-kubernetes",
+      "sudo apt-get update",
+      "sudo snap install task --classic",
       "sudo hostnamectl set-hostname \"${local.all_nodes[count.index].name}\"",
       [for node in  local.all_nodes : "sudo sh -c 'echo \"${node.ipv4_address} ${node.name}\" >> /etc/hosts'"],
+      #! best to leave task command last
+      "task setup-kubernetes",
     ])
   }
 }
